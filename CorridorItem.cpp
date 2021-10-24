b0d91925ad8ed0451953c4e7bad8a93fc3b77232
Jogechu Caicoya <caicoya@gmail.com>
2021-10-24 13:12:27 +0200

    Corridor and flats are selectables

--- CorridorItem.cpp
+++ CorridorItem.cpp
@@ -1,16 +1,29 @@
 #include "CorridorItem.h"
 #include <QPainter>
+#include <QStyleOptionGraphicsItem>
 
 
 CorridorItem::CorridorItem(const QRect& rect)
     : QGraphicsRectItem(rect)
 {
+    setFlag(ItemIsSelectable);
+    setFlag(ItemSendsGeometryChanges);
+    setCacheMode(DeviceCoordinateCache);
+    setZValue(-1);
 }
 
 
-void CorridorItem::paint(QPainter *painter, [[maybe_unused]] const QStyleOptionGraphicsItem *option, [[maybe_unused]] QWidget *widget)
+void CorridorItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, [[maybe_unused]] QWidget *widget)
 {
-    painter->setBrush(Qt::red);
-    painter->setPen(QPen(Qt::black, 2));
-    painter->drawRect(rect());
+    // TODO: painting a wider border implies to define bounding rect properly
+    //painter->setPen(QPen(Qt::black, 1));
+    painter->drawRect(boundingRect());
+    if (option->state.testFlag(QStyle::State_Selected))
+    {
+        painter->fillRect(boundingRect(), Qt::red);
+    }
+    else
+    {
+        painter->fillRect(boundingRect(), Qt::green);
+    }
 }
--- FlatItem.cpp
+++ FlatItem.cpp
@@ -1,15 +1,29 @@
 #include "FlatItem.h"
 #include <QPainter>
+#include <QStyleOptionGraphicsItem>
 
 
 FlatItem::FlatItem(const QRect& rect)
     : QGraphicsRectItem(rect)
 {
+    setFlag(ItemIsSelectable);
+    setFlag(ItemSendsGeometryChanges);
+    setCacheMode(DeviceCoordinateCache);
+    setZValue(-1);
 }
+
 
 void FlatItem::paint(QPainter *painter, [[maybe_unused]] const QStyleOptionGraphicsItem *option, [[maybe_unused]] QWidget *widget)
 {
-    painter->setBrush(Qt::yellow);
-    painter->setPen(QPen(Qt::black, 2));
-    painter->drawRect(rect());
+    // TODO: painting a wider border implies to define bounding rect properly
+    //painter->setPen(QPen(Qt::black, 1));
+    painter->drawRect(boundingRect());
+    if (option->state.testFlag(QStyle::State_Selected))
+    {
+        painter->fillRect(boundingRect(), Qt::blue);
+    }
+    else
+    {
+        painter->fillRect(boundingRect(), Qt::yellow);
+    }
 }
